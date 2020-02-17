4ec2e88356e23674094d801214a64a8484c76aed
Abdelfatah <the_pilot.hezema@yahoo.com>
2020-02-18 00:12:00 +0200

    Testing that the Burger Appears on the screen with ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,6 +9,8 @@ const burger = (props) => {
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top"/>
             <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="salad"/>
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
