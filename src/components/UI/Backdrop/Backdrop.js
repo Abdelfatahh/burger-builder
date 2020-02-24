f910d03a990c6f1f33579295d86a8ecbff614b1d
Abdelfatah <the_pilot.hezema@yahoo.com>
2020-02-24 23:56:09 +0200

    working on the backdrop component

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './backdrop.module.css'
+const backdrop = (props) => (
+    props.show ? <div className={styles.Backdrop}></div> : null
+);
+
+export default backdrop;
